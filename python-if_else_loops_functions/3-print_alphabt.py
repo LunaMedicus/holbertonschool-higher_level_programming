8c7402e932e22942e5ae2aa14a9a310a57a7bf96
Drmondo <12646@holbertonstudents.com>
2026-03-18 18:36:16 +0400

    4!

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for alpha_letters in range(97, 123):
-   if alpha_letters == ord(e) or alpha_letters == ord(q):
-         continue
+   if ord(alpha_letters) ==101  or ord(alpha_letters) == 113:
+      continue
    print("{:c}".format(alpha_letters), end="")
