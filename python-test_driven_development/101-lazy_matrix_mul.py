b866006e625b3658501e2a6be10c105336313181
Drmondo <12646@holbertonstudents.com>
2026-04-12 10:45:43 +0400

    Revert "fixes"
    This reverts commit 6be34f2e42a30c52425a0c1d4ef86d7a39c82126.

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -16,10 +16,3 @@ def lazy_matrix_mul(m_a, m_b):
         return (np.matmul(m_a, m_b))
     except TypeError:
         raise ValueError("Scalar operands are not allowed, use '*' instead")
-    except ValueError:
-        arr_a = np.array(m_a)
-        arr_b = np.array(m_b)
-        shape_a = str(arr_a.shape).replace(" ", "")
-        shape_b = str(arr_b.shape).replace(" ", "")
-        raise ValueError("shapes {} and {} not aligned: {} (dim 1) != {} (dim 0)".format(
-            shape_a, shape_b, arr_a.shape[1], arr_b.shape[0]))
